0b53ce6e4eaec8f2561573a308cd29b44e8a7cb3
Jhon Freddy Rondon <jhonfr29@gmail.com>
2019-10-28 10:36:01 -0500

    configuration zuul server

---
+++ src/test/java/com/ai/st/zuul/StZuulServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ai.st.zuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StZuulServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
