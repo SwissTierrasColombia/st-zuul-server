34f57c634de59d49f622d9301ea95a718f6928ef
shiido <jhonfr29@gmail.com>
2021-05-13 13:51:06 -0500

    chore: find deliveres endpoint added

--- src/main/java/com/ai/st/zuul/oauth/ResourceServerConfig.java
+++ src/main/java/com/ai/st/zuul/oauth/ResourceServerConfig.java
@@ -192,6 +192,7 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
 				// microservice quality
 				.antMatchers(HttpMethod.POST, "/api/quality/v1/deliveries").hasAnyRole(ROLE_OPERATOR)
+				.antMatchers(HttpMethod.GET, "/api/quality/v1/deliveries").hasAnyRole(ROLE_OPERATOR, ROLE_MANAGER)
 				.antMatchers(HttpMethod.PUT, "/api/quality/v1/deliveries/{deliveryId}/products").hasAnyRole(ROLE_OPERATOR)
 				.antMatchers(HttpMethod.GET, "/api/quality/v1/products").hasAnyRole(ROLE_MANAGER, ROLE_OPERATOR)
 				
