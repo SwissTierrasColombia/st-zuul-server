e80ec7f0dc2d2f9243e8e8e5e635ae14cf6cc0d8
shiido <jhonfr29@gmail.com>
2021-05-18 16:15:16 -0500

    chore: remove-product-from-delivery endpoint added

--- src/main/java/com/ai/st/zuul/oauth/ResourceServerConfig.java
+++ src/main/java/com/ai/st/zuul/oauth/ResourceServerConfig.java
@@ -200,6 +200,7 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 				.antMatchers(HttpMethod.GET, "/api/quality/v1/deliveries/{deliveryId}/products/{deliveryProductId}/attachments").hasAnyRole(ROLE_OPERATOR, ROLE_MANAGER)
 				.antMatchers(HttpMethod.DELETE, "/api/quality/v1/deliveries/{deliveryId}/products/{deliveryProductId}/attachments/{attachmentId}").hasAnyRole(ROLE_OPERATOR)
 				.antMatchers(HttpMethod.GET, "/api/quality/v1/deliveries/{deliveryId}/products/{deliveryProductId}/attachments/{attachmentId}/download").hasAnyRole(ROLE_OPERATOR, ROLE_MANAGER)
+				.antMatchers(HttpMethod.DELETE, "/api/quality/v1/deliveries/{deliveryId}/products/{deliveryProductId}").hasAnyRole(ROLE_OPERATOR)
 				.antMatchers(HttpMethod.GET, "/api/quality/v1/products").hasAnyRole(ROLE_MANAGER, ROLE_OPERATOR)
 
 				/*
