e0dc527e9d3dc64392c288759fde9cfff65c1a84
shiido <jhonfr29@gmail.com>
2021-05-18 17:23:37 -0500

    chore: add-product-to-delivery endpoint updated

--- src/main/java/com/ai/st/zuul/oauth/ResourceServerConfig.java
+++ src/main/java/com/ai/st/zuul/oauth/ResourceServerConfig.java
@@ -194,7 +194,7 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 				.antMatchers(HttpMethod.POST, "/api/quality/v1/deliveries").hasAnyRole(ROLE_OPERATOR)
 				.antMatchers(HttpMethod.GET, "/api/quality/v1/deliveries").hasAnyRole(ROLE_OPERATOR, ROLE_MANAGER)
 				.antMatchers(HttpMethod.GET, "/api/quality/v1/deliveries/{deliveryId}").hasAnyRole(ROLE_OPERATOR, ROLE_MANAGER)
-				.antMatchers(HttpMethod.PUT, "/api/quality/v1/deliveries/{deliveryId}/products").hasAnyRole(ROLE_OPERATOR)
+				.antMatchers(HttpMethod.POST, "/api/quality/v1/deliveries/{deliveryId}/products").hasAnyRole(ROLE_OPERATOR)
 				.antMatchers(HttpMethod.GET, "/api/quality/v1/deliveries/{deliveryId}/products").hasAnyRole(ROLE_OPERATOR, ROLE_MANAGER)
 				.antMatchers(HttpMethod.POST, "/api/quality/v1/deliveries/{deliveryId}/products/{deliveryProductId}/attachments").hasAnyRole(ROLE_OPERATOR)
 				.antMatchers(HttpMethod.GET, "/api/quality/v1/deliveries/{deliveryId}/products/{deliveryProductId}/attachments").hasAnyRole(ROLE_OPERATOR, ROLE_MANAGER)
