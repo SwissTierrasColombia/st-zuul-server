49e1ea840b870a60da6814197a6d8426827f2fdf
shiido <jhonfr29@gmail.com>
2021-10-07 09:16:42 -0500

    feat: Service health update

--- src/main/java/com/ai/st/zuul/controllers/IndexGetController.java
+++ src/main/java/com/ai/st/zuul/controllers/IndexGetController.java
@@ -1,17 +1,20 @@
 package com.ai.st.zuul.controllers;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class IndexGetController {
 
     @GetMapping(value = "/", produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseBody
-    public ResponseEntity<?> health() {
-        return new ResponseEntity<>("OK", HttpStatus.OK);
+    public Map<String, String> health() {
+        HashMap<String, String> map = new HashMap<>();
+        map.put("status", "st_on");
+        return map;
     }
 
 }
